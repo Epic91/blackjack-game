30babb01a8f78f741e63fb359bb561b0363362f6
Epic91 <hernandeze791@gmail.com>
2021-10-07 14:01:12 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// state
+let cards = 0;
+let player = {
+    name: "Evelyn"
+}
+
+
+
+// stargame()
+    // create firstCard and secondCard
+
+
+//  newCard()
+
+
+// getRandom()
+
+
+
+
+// RULES:
+    //  21 === "Blackjack!"
+    //  < 21 === "new card"
+    // > 21 === "lose"
