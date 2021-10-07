cd8b03e2556db62db6b709e49acb28b46ddc0e46
Epic91 <hernandeze791@gmail.com>
2021-10-07 14:19:02 -0500

    initialize variable and created functions

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
-// state
-let cards = 0;
+// initialize & set variables
 let player = {
-    name: "Evelyn"
+    name: "Evelyn",
+    chips: 200
+}
+let cards = 0;
+let sum = 0;
+let isAlive = false;
+let hasBlackJack = false;
+let message = "";
+
+// Elements
+let messageEl = document.getElementById("message-el");
+let cardsEl = document.getElementById("cards-el");
+let sumEl = document.getElementById("sum-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = `${player.name} : $${player.chips}`
+
+// getRandom()
+function getRandomCard() {
+    let randomNum =  Math.floor(Math.random() * 13) + 1;
+
+    // Conditional:
+    if ( randomNum === 1) {
+        return 11
+    } else if ( randomNum > 10) {
+        return 10
+    } else {
+        return randomNum
+    }
 }
 
-
-
 // stargame()
+function startGame() {
+    let firstCard = getRandomCard();
+}
     // create firstCard and secondCard
 
 
 //  newCard()
 
 
-// getRandom()
 
 
 
